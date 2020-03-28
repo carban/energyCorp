b60735cb10864634186faa3a98360755b5218bba
esneidermanzano <esneidermanzano@gmail.com>
2020-03-28 12:07:00 -0500

    travis test 4

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>  Texto de prueba xd.
+          Edit <code>src/App.js</code>  jajaja stemen xd.
         </p>
         <a
           className="App-link"
